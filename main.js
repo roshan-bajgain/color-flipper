7604726716b88346c4bcf2a5271b78a3523a22c8
Roshan Bajgain <roshan.bazgain@gmail.com>
2021-04-10 20:27:50 -0700

    add all color

--- main.js
+++ main.js
@@ -3,9 +3,15 @@ const btn = document.getElementById('btn');
 const colorElement = document.querySelector(".color");
 btn.addEventListener("click", function(){
     const randomnumber = getrandomnumber();
-    document.body.style.backgroundColor = colors[randomnumber];
-    colorElement.textContent = colors[randomnumber];
+    const red = getrandomnumber();
+    const green = getrandomnumber();
+    const blue = getrandomnumber();
+
+    document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    colorElement.textContent = `rgb(${red}, ${green}, ${blue})`;
+
 });
+
 function getrandomnumber(){
-    return Math.floor(Math.random()*colors.length);
+    return Math.floor(Math.random()*255);
 }
